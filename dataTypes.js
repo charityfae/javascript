22d7bab9decec1bcc33c1e6e665830db1034d8f0
Ojera Charity Faith <faeojera@gmail.com>
2025-02-18 10:51:26 +0300

    js

---
+++ dataTypes.js
@@ -0,0 +1,20 @@
+
+//number-a numerical value eg 1,2,3...  
+
+//array-a list of items
+//object-a collection of key value pairs
+
+
+//NaN-not a number
+//infinity-a number that is greater than any other number
+//date
+//function
+//real
+//integer
+//float
+//double
+
+
+
+
+
---
+++ index.js
@@ -0,0 +1,10 @@
+console.log(1+1);
+
+console.log(1+2);
+
+console.log(1+3);
+
+console.log(1+4);
+
+
+
