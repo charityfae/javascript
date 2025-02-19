00392b53abb72924571be8619aa590b1b3037cb9
Ojera Charity Faith <faeojera@gmail.com>
2025-02-19 10:02:48 +0300

    nonprimitive

---
+++ nonprimitive.js
@@ -0,0 +1,20 @@
+//Array
+//Object
+
+
+let user = {
+    lastname: "charity",
+    age: 25,
+    isMarried: true,
+    city: "nairobi",
+    email: "faeojer@gmail.com"}
+    console.log(user);
+    console.log(user.lastname);
+
+    let shoes = ["nike", "adidas", "puma", "reebok"];
+    console.log(shoes);
+
+    //math object
+    const PI=Math.round(Math.PI);
+    console.log(PI);
+    
