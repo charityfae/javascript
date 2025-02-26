d7ba08ed29f46b32999e13e188d395b966675262
Ojera Charity Faith <faeojera@gmail.com>
2025-02-26 10:18:36 +0300

    arrays and loops

---
+++ week2/loops.js
@@ -0,0 +1,16 @@
+//for(initialization, condition, increment/decrement){
+//any code runs here
+//}
+ for (let number = 0; number<=100; number++){
+    console.log(number);
+ }
+ const number = 5; // Change this to any number you want
+for (let i = 1; i <= 12; i++) {
+    console.log(`${number} * ${i} = ${number * i}`);
+}
+ let numbers = [1,2,3,4,5];
+ let sum = 0;
+ for (let i=0; i<numbers.length; i++) {
+    sum = sum + numbers[i];
+    console.log(sum);
+ }
